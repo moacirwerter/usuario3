3643a1f5e5d77c2a470a07d31488c91397339377
Moacir Werter <moacirwerter@gmail.com>
2026-08-31 16:35:38 -0300

    commit inicial

---
+++ src/main/java/com/projeto3/usuario3/Usuario3Application.java
@@ -0,0 +1,13 @@
+package com.projeto3.usuario3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Usuario3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Usuario3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto3/usuario3/Usuario3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto3.usuario3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Usuario3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
