dc6c3b161e0eccdf9f8fbfa988dea89b0843f59a
Moacir Werter <moacirwerter@gmail.com>
2026-09-14 16:20:06 -0300

    fix: corrige erro 403 na rota de cadastro e unifica filtros jwt

--- src/main/java/com/projeto3/usuario3/controller/UsuarioController.java
+++ src/main/java/com/projeto3/usuario3/controller/UsuarioController.java
@@ -2,11 +2,13 @@ package com.projeto3.usuario3.controller;
 
 import com.projeto3.business.dto.usuarioDTO;
 import com.projeto3.business.UsuarioService;
+import com.projeto3.entity.Usuario;
+import com.projeto3.security.JwtUtil;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 import lombok.RequiredArgsConstructor;
 
 
@@ -17,9 +19,32 @@ import lombok.RequiredArgsConstructor;
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
 
     @PostMapping
     public ResponseEntity<usuarioDTO> salvaUsuario(@RequestBody usuarioDTO usuarioDTO) {
         return ResponseEntity.ok(usuarioService.salvaUsuario(usuarioDTO));
+    }
+    @PostMapping("/login")
+    public String login(@RequestBody usuarioDTO usuarioDTO) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        usuarioDTO.getEmail(),
+                        usuarioDTO.getSenha()
+                )
+        );
+
+        return "Bearer " + jwtUtil.generateToken(authentication.getName());
+    }
+    @GetMapping
+    public ResponseEntity<Usuario> buscarPorEmail(@RequestParam("email") String email){
+        return ResponseEntity.ok(usuarioService.buscarusuarioPorEmail(email));
+    }
+    @DeleteMapping("/{email}")
+    public ResponseEntity<Void> deleteUsuarioPorEmail(@PathVariable String email){
+        usuarioService.deleteUsuarioPorEmail(email);
+        return ResponseEntity.ok().build();
+
     }
 }
